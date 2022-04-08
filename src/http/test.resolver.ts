daa22bc9c0d893b943e58e363660c68a4191378f
Luciano Ferreira <luciano2023silva@gmail.com>
2022-04-07 23:45:27 -0300

    fix: test resolver

--- src/http/test.resolver.ts
+++ src/http/test.resolver.ts
@@ -1,7 +1,5 @@
-import { UseGuards } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
 import { PrismaService } from 'src/database/prisma/prisma.service';
-import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 
 @Resolver()
 export class TestResolver {
